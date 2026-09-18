e07791075b5b4a6cd67a4a6d6d797cec4b5eef25
Mary Oreofejesu Imarenezor <maryimarenezor13@gmail.com>
2026-09-18 13:42:33 -0500

    made my sectiongit status

--- src/Person_4/Component.jsx
+++ src/Person_4/Component.jsx
@@ -2,9 +2,11 @@ import "./Component.css"
 
 function Person4(){
     return(
-        <>
-        
-        </>
+        <div id="mary-container">
+            <h3>Hello world!!</h3>
+            <h3>The Name's Mary!!</h3>
+            <p>hacking is ok as long as its for a good cause!! :D</p>
+        </div>
     )
 }
 
