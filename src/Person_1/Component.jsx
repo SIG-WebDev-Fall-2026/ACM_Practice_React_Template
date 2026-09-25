10351b69217d4f169e050d90a24b4cc0fd604e77
nnolmos1 <nnolmos1@gmail.com>
2026-09-24 20:52:49 -0500

    Updated Person_1

--- src/Person_1/Component.jsx
+++ src/Person_1/Component.jsx
@@ -1,9 +1,38 @@
 import "./Component.css"
+import React from "react";
 
 function Person1(){
+
+    const [guess, setGuess] = React.useState(0);
+
+    function handleChange(e){
+        setGuess(e.target.value);
+    }
+
     return(
         <>
-            
+            <div className = "person1">
+                <p> ✦⟡✦ NOAH .O1</p>   
+
+                <div className = "person1-info"> 
+                    <ul>
+                        <li> Sophomore in CS + Design</li>
+                        <li> Web Design, UI/UX, Frontend</li>
+                    </ul>   
+
+                    <div className = "person1-guess-section">
+                        <label for="guess">Guess my name:</label>
+                        <input id="guess" type="text" value={guess} onChange={handleChange} />
+
+                        {guess && (
+                            <p className = "person1-guess-response">
+                                {guess.toLowerCase() === "noah" ? "Correct! ✔" : "Try again :("}
+                            </p> 
+                        
+                        )}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
