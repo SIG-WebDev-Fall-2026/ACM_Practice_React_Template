63825ace67d77827e064929280efe8c6eb7ae71a
gobbledygook-kazoo <vincentm2633@gmail.com>
2026-09-25 13:25:20 -0500

    Cell 2 Update

--- src/Person_2/Component.jsx
+++ src/Person_2/Component.jsx
@@ -3,7 +3,9 @@ import "./Component.css"
 function Person2(){
     return(
         <>
-        
+        <h3> This is cell 2 !? </h3>
+        <div className= "Person_2"> <p> The quick brown fox jumped over the lazy dog. </p> </div>
+        <button id= "button"> Button </button>
         </>
     )
 }
